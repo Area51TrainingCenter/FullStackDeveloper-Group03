cf8e54a9095005fa6984be0de85322f999fcfd25
alumno <alumno@MacBook-Pro-de-Usuario.local>
2018-01-11 22:17:13 -0500

    Clase02

---
+++ Javascript/Clase02/04-carrusel.js
@@ -0,0 +1,24 @@
+const listaClases = ["slide04", "slide03", "slide02", "slide01", "slide05", "slide06", "slide07"]
+
+const slides = document.getElementsByClassName("slide")
+
+const listaSlides = Array.from(slides)
+
+listaSlides.forEach(function(elemento, indice){
+	elemento.classList.add(listaClases[indice])
+})
+
+function mover(){
+	const primerElemento = listaClases.pop()
+	listaClases.unshift(primerElemento)
+	
+	listaSlides.forEach(function(elemento, indice){
+		elemento.classList.remove("slide01", "slide02", "slide03", "slide04", "slide05", "slide06", "slide07")
+		elemento.classList.add(listaClases[indice])
+	})
+}
+
+setInterval(mover, 1000)
+
+//mover()
+
