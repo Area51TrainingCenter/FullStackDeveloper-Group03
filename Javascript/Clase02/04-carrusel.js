7b45deab20778fd111474cdfee2f81d6278238e1
Sergio <shidalgo@outlook.com>
2018-01-15 15:54:49 -0500

    Clase02

--- Javascript/Clase02/04-carrusel.js
+++ Javascript/Clase02/04-carrusel.js
@@ -4,21 +4,19 @@ const slides = document.getElementsByClassName("slide")
 
 const listaSlides = Array.from(slides)
 
-listaSlides.forEach(function(elemento, indice){
+listaSlides.forEach(function (elemento, indice) {
 	elemento.classList.add(listaClases[indice])
 })
 
-function mover(){
-	const primerElemento = listaClases.pop()
-	listaClases.unshift(primerElemento)
-	
-	listaSlides.forEach(function(elemento, indice){
+function mover() {
+	const ultimoElemento = listaClases.pop()
+	listaClases.unshift(ultimoElemento)
+
+	listaSlides.forEach(function (elemento, indice) {
 		elemento.classList.remove("slide01", "slide02", "slide03", "slide04", "slide05", "slide06", "slide07")
 		elemento.classList.add(listaClases[indice])
 	})
 }
 
 setInterval(mover, 1000)
-
 //mover()
-
--- Javascript/README.md
+++ Javascript/README.md
@@ -1,3 +1,4 @@
 # JavaScript ES6 (ES2015)
 ### Sumario
 1. [Variables, Elevación de variables, Bucles](https://github.com/Area51TrainingCenter/FullStackDeveloper-Group03/tree/master/Javascript/Clase01)
+2. [Arreglos, selectores, modificar elementos html, carrusel](https://github.com/Area51TrainingCenter/FullStackDeveloper-Group03/tree/master/Javascript/Clase02)
