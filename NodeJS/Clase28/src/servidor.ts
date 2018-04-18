11509a42100ee9fe1ce613bdc24830b37961078b
Admin <admin@A51-04.local>
2018-04-17 22:06:28 -0500

    Clase28

--- NodeJS/Clase28/src/servidor.ts
+++ NodeJS/Clase28/src/servidor.ts
@@ -1,22 +1,27 @@
 // Importaciones
 import express = require("express")
 import { Request, Response, NextFunction } from "express"
+import morgan = require("morgan")
+import bodyParser = require("body-parser")
+import methodOverride = require("method-override")
+import {ruteador as rutaHome} from "../rutas/index"
+import {ruteador as rutaUsuarios} from "../rutas/usuarios"
 
 // Declaraciones
 const app = express()
 app.set("view engine", "pug")
 app.set("views", "./vistas")
 
-// Rutas
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
+// Middleware
+app.use(morgan("dev"))
+app.use(express.static("./publico"))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(methodOverride("_method"))
 
-    //console.log(__dirname)
-    res
-        .status(200)
-        .type("text/html")
-        .render("home")
-        //.sendFile("/Users/administrador/Desktop/FullStackDeveloper-Group03/NodeJS/Clase28/vistas/home.html")
-})
+// Rutas
+app.use("/", rutaHome)
+app.use("/usuarios", rutaUsuarios)
 
 // Servidor
 app.listen(4000, () => {
