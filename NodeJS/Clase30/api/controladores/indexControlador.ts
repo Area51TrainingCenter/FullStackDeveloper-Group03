930b08c95a7ef3c37ed6de6a174d5b873ac27d07
Admin <admin@A51-04.local>
2018-04-24 22:10:54 -0500

    Clase30

--- NodeJS/Clase30/api/controladores/indexControlador.ts
+++ NodeJS/Clase30/api/controladores/indexControlador.ts
@@ -1,8 +1,17 @@
 import { Request, Response, NextFunction } from "express"
+import { cine } from '../modelos/cine'
 
 const controlador = {
 	home: (req: Request, res: Response) => {
-		res.render("Home", { titulo: "Home" })
+		res.render("home", { titulo: "Home" })
+	},
+	agregar: (req: Request, res: Response) => {
+		res.render("editarCine", { titulo: "Agregar" })
+	},
+	insertar: async (req: Request, res: Response, next: NextFunction) => {
+		const Cine = new cine(req.body)
+		await Cine.save()
+		res.redirect("/")
 	}
 }
 
