13ee195cf0a3492f315016e469fe44c9bc1265c4
area51 <admin@Mac-mini-de-area51.local>
2018-05-08 22:11:41 -0500

    Clase33

--- NodeJS/Clase33/rutas/cinesRutas.ts
+++ NodeJS/Clase33/rutas/cinesRutas.ts
@@ -2,10 +2,11 @@ import { Request, Response, NextFunction } from "express"
 import express = require("express")
 import { controlador } from "../api/controladores/cinesControlador"
 import { manejadorErrores } from "../errores/manejador"
+import { politica } from "../api/politicas/autenticacion";
 
 const ruteador = express.Router()
 
-ruteador.get("/", manejadorErrores.catch(controlador.listar))
+ruteador.get("/", politica, manejadorErrores.catch(controlador.listar))
 ruteador.post("/", manejadorErrores.catch(controlador.insertar))
 ruteador.put("/:id", manejadorErrores.catch(controlador.modificar))
 ruteador.delete("/:id", manejadorErrores.catch(controlador.eliminar))
